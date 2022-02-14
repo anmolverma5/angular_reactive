8de96642ebc149831bbadef82d22ad07d2338310
Anmol Verma <anmolverma800@gmail.com>
2022-02-15 00:17:19 +0530

    Now only confirm validation pending

--- frontend/src/app/components/signup/signup.component.ts
+++ frontend/src/app/components/signup/signup.component.ts
@@ -1,6 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder, Validator, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
 import { Users } from 'src/app/model/Users';
+
+
+function confirm_password(): ValidatorFn{
+  return(c: AbstractControl): {[key:string]: boolean} | null | any => {
+    let password = c.get('password');
+    let cpassword = c.get('cpassword');
+    if (password?.pristine || cpassword?.pristine){
+      return null;
+    }
+    if(password?.value === cpassword?.value){
+    return false;
+    }
+    return {'match': true};
+  };
+}
+
 
 @Component({
   selector: 'app-signup',
@@ -9,18 +25,31 @@ import { Users } from 'src/app/model/Users';
 })
 export class SignupComponent implements OnInit {
   signupForm!: FormGroup;
-  save: any;
   users = new Users();
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    this.signupForm = new FormGroup({
-      firstname: new FormControl(),
-      lastname: new FormControl(),
-      email: new FormControl(),
-      password: new FormControl()
-    });
+    this.signupForm = this.fb.group({
+      firstname: ['', [Validators.required, Validators.minLength(3)]],
+      lastname: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['',[ Validators.required, Validators.email]],
+      passwordGroup: this.fb.group({
+        password: ['', [Validators.required,Validators.minLength(6)]],
+      cpassword: ['', [Validators.required,Validators.minLength(6)]]
+      }, {validators: confirm_password}),      
+    })
+    // this.signupForm = new FormGroup({
+    //   firstname: new FormControl(),
+    //   lastname: new FormControl(),
+    //   email: new FormControl(),
+    //   password: new FormControl(),
+    //   cpassword: new FormControl()
+    // });
+  }
+  save(): void {
+    console.log(this.signupForm);
+    console.log('Saved: ' + JSON.stringify(this.signupForm.value));
   }
 
 }
