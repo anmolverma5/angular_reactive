aa1140cc8947db614bcf143687ebe3cd579284c5
Anmol Verma <anmolverma800@gmail.com>
2022-02-27 18:44:08 +0530

    Json_web_token_created_successfully

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Users } from 'src/app/model/Users';
 import { LoginServiceService } from './login-service.service';
 
@@ -17,8 +17,8 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
-      email: ['anmol@gmail.com'],
-      password: ['12345678']
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
     })
   }
   save() {
